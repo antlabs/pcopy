923eebaae4c74f74ac4cf8b865f0f884dc8ffd6f
guonaihong <guonaihong@qq.com>
2020-04-22 20:02:25 +0800

    新增MaxDepth接口

--- deepcopy.go
+++ deepcopy.go
@@ -4,6 +4,52 @@ import (
 	"reflect"
 )
 
+const (
+	noDepthLimited = -1
+	noTagLimit     = ""
+)
+
+// deepCopy结构体
+type deepCopy struct {
+	maxDepth int
+	tagName  string
+	dst      interface{}
+	src      interface{}
+}
+
+// 设置dst, src数据源
+func Copy(dst, src interface{}) *deepCopy {
+	if dst == nil || src == nil {
+		return nil
+	}
+
+	d := deepCopy{maxDepth: noDepthLimited, dst: dst, src: src}
+	return &d
+}
+
+// 设置最多递归的层次
+func (d *deepCopy) MaxDepth(maxDepth int) *deepCopy {
+	d.maxDepth = maxDepth
+	return d
+}
+
+// 设置tag name，结构体的tag等于RegisterTagName注册的tag，才会copy值
+func (d *deepCopy) RegisterTagName(tagName string) *deepCopy {
+	d.tagName = tagName
+	return d
+}
+
+// 需要的tag name
+func (d *deepCopy) needTagName(curTabName string) bool {
+	return d.tagName == curTabName
+}
+
+// Do() 开干
+func (d *deepCopy) Do() error {
+	return d.deepCopy(reflect.ValueOf(d.dst), reflect.ValueOf(d.src), 0)
+}
+
+// 取最小值
 func min(a, b int) int {
 	if a > b {
 		return b
@@ -20,9 +66,13 @@ func isArraySlice(v reflect.Value) bool {
 	return false
 }
 
-func deepCopy(dst, src reflect.Value) error {
+func (d *deepCopy) deepCopy(dst, src reflect.Value, depth int) error {
 
 	if src.IsZero() {
+		return nil
+	}
+
+	if d.maxDepth != noDepthLimited && depth > d.maxDepth {
 		return nil
 	}
 
@@ -34,15 +84,15 @@ func deepCopy(dst, src reflect.Value) error {
 		}
 
 		if dst.Kind() == reflect.Slice && dst.Len() == 0 && src.Len() > 0 {
-			// MakeSlice的类型用reflect.SliceOf(src.Index(0).Type()),不用src.Type()的原因
-			// 这里src的类型可能是array和slice。我们要的是src元素T的 []T类型,就能不使用src.Type()
+			// MakeSlice的类型用reflect.SliceOf(src.Index(0).Type()),而不用src.Type()的原因
+			// 这里src的类型可能是array和slice。而需要的是src元素T[0]的 slice类型, 使用src.Type()会拿到T的array和slice类型
 			newDst := reflect.MakeSlice(reflect.SliceOf(src.Index(0).Type()), src.Len(), src.Len())
 			dst.Set(newDst)
 		}
 
 		l := min(dst.Len(), src.Len())
 		for i := 0; i < l; i++ {
-			if err := deepCopy(dst.Index(i), src.Index(i)); err != nil {
+			if err := d.deepCopy(dst.Index(i), src.Index(i), depth); err != nil {
 				return err
 			}
 		}
@@ -62,37 +112,54 @@ func deepCopy(dst, src reflect.Value) error {
 			k := iter.Key()
 			v := iter.Value()
 
-			dst.SetMapIndex(k, v)
+			newVal := reflect.New(v.Type()).Elem()
+			if err := d.deepCopy(newVal, v, depth); err != nil {
+				return err
+			}
+
+			dst.SetMapIndex(k, newVal)
 		}
 
 	case reflect.Func:
+		/*
+			结构体成员如果是一个函数变量, dst.IsNil()会返回true
+			if dst.IsNil() {
+				fmt.Printf("hell world\n")
+				newFunc := reflect.New(src.Type())
+				dst = newFunc.Elem()
+			}
+		*/
+
+		dst.Set(src)
 	case reflect.Struct:
 
 		typ := src.Type()
 		for i, n := 0, src.NumField(); i < n; i++ {
 			sf := typ.Field(i)
-			if err := deepCopy(dst.FieldByName(sf.Name), src.Field(i)); err != nil {
+			if !d.needTagName(sf.Tag.Get(d.tagName)) {
+				continue
+			}
+
+			if err := d.deepCopy(dst.FieldByName(sf.Name), src.Field(i), depth+1); err != nil {
 				return err
 			}
 		}
 
 	case reflect.Interface:
+		if src.IsNil() {
+			return nil
+		}
+
+		return d.deepCopy(dst.Elem(), src.Elem(), depth)
 	case reflect.Ptr:
 		if dst.Kind() == reflect.Ptr {
 			dst = dst.Elem()
 		}
-		return deepCopy(dst, src.Elem())
+
+		return d.deepCopy(dst, src.Elem(), depth)
 	default:
 		dst.Set(src)
 	}
 
 	return nil
-}
-
-func DeepCopy(dst, src interface{}) error {
-	if dst == nil || src == nil {
-		return nil
-	}
-
-	return deepCopy(reflect.ValueOf(dst), reflect.ValueOf(src))
 }
--- deepcopy_test.go
+++ deepcopy_test.go
@@ -1,9 +1,18 @@
 package deepcopy
 
 import (
+	"fmt"
 	"github.com/stretchr/testify/assert"
 	"testing"
+	"unsafe"
 )
+
+type testCase struct {
+	need interface{}
+	got  interface{}
+}
+
+// 考虑dst为空指针情况
 
 // 测试slice拷贝到array
 // TODO更多的情况
@@ -21,7 +30,7 @@ func Test_SliceToArray(t *testing.T) {
 		A: []int{1, 2, 3, 4, 5},
 	}
 
-	DeepCopy(&d, &s)
+	Copy(&d, &s).Do()
 	assert.Equal(t, d, dst{A: [3]int{1, 2, 3}})
 }
 
@@ -41,7 +50,7 @@ func Test_ArrayToSlice(t *testing.T) {
 		A: [3]int{1, 2, 3},
 	}
 
-	DeepCopy(&d, &s)
+	Copy(&d, &s).Do()
 	assert.Equal(t, d, dst{A: []int{1, 2, 3}})
 }
 
@@ -72,6 +81,76 @@ func Test_MapToMap(t *testing.T) {
 		A: a,
 	}
 
-	DeepCopy(&d, &s)
+	Copy(&d, &s).Do()
 	assert.Equal(t, d, dst{A: a, B: b})
+}
+
+func Test_FuncToFunc(t *testing.T) {
+	type dst struct {
+		A func()
+	}
+
+	type src struct {
+		A func()
+	}
+
+	d := dst{}
+	s := src{
+		A: func() { fmt.Printf("hello") },
+	}
+
+	Copy(&d, &s).Do()
+
+	//如果指向的是同一个地址的函数，注释的方法是不行的
+	//assert.Equal(t, d.A, s.A)
+	assert.Equal(t, *(*uintptr)(unsafe.Pointer(&d.A)), *(*uintptr)(unsafe.Pointer(&s.A)))
+}
+
+// 最大深度
+func Test_MaxDepth(t *testing.T) {
+	type depth struct {
+		First string
+		Data  struct {
+			Result string
+		}
+		Err struct {
+			ErrMsg struct {
+				Message string
+			}
+		}
+	}
+
+	src := depth{}
+	src.First = "first"
+	src.Data.Result = "test"
+	src.Err.ErrMsg.Message = "good"
+
+	for _, tc := range []testCase{
+		func() testCase {
+			d := depth{}
+			Copy(&d, &src).MaxDepth(1).Do()
+			need := depth{}
+			need.First = "first"
+			return testCase{got: d, need: need}
+		}(),
+		func() testCase {
+			d := depth{}
+			Copy(&d, &src).MaxDepth(2).Do()
+			need := depth{}
+			need.First = "first"
+			need.Data.Result = "test"
+			return testCase{got: d, need: need}
+		}(),
+		func() testCase {
+			d := depth{}
+			Copy(&d, &src).MaxDepth(3).Do()
+			need := depth{}
+			need.First = "first"
+			need.Data.Result = "test"
+			need.Err.ErrMsg.Message = "good"
+			return testCase{got: d, need: need}
+		}(),
+	} {
+		assert.Equal(t, tc.need, tc.got)
+	}
 }
