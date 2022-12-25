821d2da39da2d696f183d23b3656c89257f46cca
guonaihong <guonaihong@qq.com>
2022-12-25 22:13:17 +0800

    修复time.Time不能拷贝的问题 (#10)

---
+++ deepcopy_time_test.go
@@ -0,0 +1,27 @@
+package deepcopy
+
+import (
+	"testing"
+	"time"
+
+	"github.com/stretchr/testify/assert"
+)
+
+type testCaseWithTime1 struct {
+	T1 time.Time
+	I  int
+}
+
+type testCaseWithTime2 struct {
+	T1 time.Time
+	I  int
+}
+
+func Test_Time(t *testing.T) {
+	var t1 testCaseWithTime1
+	t2 := testCaseWithTime2{T1: time.Now(), I: 3}
+
+	Copy(&t1, &t2).Do()
+	assert.Equal(t, t1.T1, t2.T1)
+	assert.Equal(t, t1.I, t2.I)
+}
