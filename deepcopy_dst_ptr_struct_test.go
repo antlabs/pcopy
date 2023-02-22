49eb0d1866b2db56ee0db9ec0ffb6688622d66af
guonaihong <guonaihong@qq.com>
2023-02-22 11:14:38 +0800

    Add struct ptr (#11)
    * dst有指针类型结构体
    
    * fix

---
+++ deepcopy_dst_ptr_struct_test.go
@@ -0,0 +1,35 @@
+package deepcopy
+
+import (
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+)
+
+type TestDstStructPtr_Dst struct {
+	TestDstPtr *struct {
+		X int
+		Y int
+	}
+}
+
+type TestDstPtr2 struct {
+	X int
+	Y int
+}
+
+type TestDstStructPtr_Src struct {
+	TestDstPtr TestDstPtr2
+}
+
+func Test_DstPtr_Struct(t *testing.T) {
+	var dst TestDstStructPtr_Dst
+	src := TestDstStructPtr_Src{
+		TestDstPtr: TestDstPtr2{X: 3, Y: 4},
+	}
+
+	Copy(&dst, &src).Do()
+	assert.NotNil(t, dst.TestDstPtr)
+	assert.Equal(t, dst.TestDstPtr.X, 3)
+	assert.Equal(t, dst.TestDstPtr.Y, 4)
+}
