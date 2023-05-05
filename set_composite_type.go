d249bc7760a40208c70e451e639b8213db910da0
guonaihong <guonaihong@qq.com>
2023-05-06 00:06:10 +0800

    up

--- set_composite_type.go
+++ set_composite_type.go
@@ -33,16 +33,24 @@ func setCompositeSliceStruct(dstType, srcType reflect.Type, dst, src unsafe.Poin
 		dstVal.Set(reflect.MakeSlice(dstType, srcVal.Len(), srcVal.Len()))
 	}
 
-	// dstVal.Elem().Type().Size()
+	offset := dstVal.Elem().Type().Size()
 	subDstType := dstVal.Elem().Type().Elem()
 	subSrcType := srcVal.Elem().Type().Elem()
 	key := dstSrcType{dst: subDstType, src: subSrcType}
 
+	// 子元素项目如果是已经缓存的类型，直接调用缓存的函数
 	exits := getSetFromCacheAndRun(key, dst, src, opt)
 	if exits {
+		for i := 1; i < srcVal.Len(); i++ {
+			exits = getSetFromCacheAndRun(key, addOffset(dst, offset, i), addOffset(src, offset, i), opt)
+			if !exits {
+				panic(fmt.Sprintf("not support type:%v", key))
+			}
+		}
 		return
 	}
 
+	// TODO 这里进反射逻辑
 	for i := 0; i < srcVal.Len(); i++ {
 		dstVal.Index(i).Set(srcVal.Index(i))
 	}
