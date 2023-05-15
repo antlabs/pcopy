28637cab016d159bc8497d3260ee78e55dcd763c
guonaihong <guonaihong@qq.com>
2023-05-15 23:22:07 +0800

    合并一个测试文件

--- dcopy_ptr_double_basetype_test.go
+++ dcopy_ptr_double_basetype_test.go
@@ -176,3 +176,57 @@ func Test_DoublePtr_BaseType3(t *testing.T) {
 		assert.Equal(t, local_test_BaseType_DoublePtr_Dst, dst)
 	}
 }
+
+type Msg struct {
+	FromUid int32  `protobuf:"varint,1,opt,name=from_uid,json=fromUid,proto3" json:"from_uid,omitempty"`
+	ToUid   int32  `protobuf:"varint,2,opt,name=to_uid,json=toUid,proto3" json:"to_uid,omitempty"`
+	Msg     string `protobuf:"bytes,3,opt,name=msg,proto3" json:"msg,omitempty"`
+}
+
+// 测试src是双指针的情况
+// 为了提升dcopy库的兼容性, src是双指针, dst是单指针, 也可以正常拷贝
+func Test_Src_DoublePtr_Test(t *testing.T) {
+	m1 := Msg{FromUid: 1, ToUid: 2, Msg: "1to2"}
+
+	var opts []Option
+	for _, b := range []bool{true, false} {
+		if b {
+			opts = append(opts, WithUsePreheat())
+		} else {
+			opts = []Option{}
+		}
+		// 单指针
+		m1Ptr := &m1
+
+		var m2 Msg
+		// dst(m2) 是单指针
+		// src(m1Ptr) 是双指针
+		err := Copy(&m2, &m1Ptr, opts...)
+
+		assert.NoError(t, err)
+		assert.Equal(t, m1, m2)
+	}
+}
+
+func Test_Src_DoublePtr_Test_CopyEx(t *testing.T) {
+	m1 := Msg{FromUid: 1, ToUid: 2, Msg: "1to2"}
+
+	// 单指针
+	m1Ptr := &m1
+
+	var opts []Option
+	for _, b := range []bool{true, false} {
+		if b {
+			opts = append(opts, WithUsePreheat())
+		} else {
+			opts = []Option{}
+		}
+		var m2 Msg
+		// dst(m2) 是单指针
+		// src(m1Ptr) 是双指针
+		err := Copy(&m2, &m1Ptr, opts...)
+
+		assert.NoError(t, err)
+		assert.Equal(t, m1, m2)
+	}
+}
